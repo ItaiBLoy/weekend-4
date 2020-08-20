a5e028d4474dbca602eb62746eb2a39f06721885
Itai <ezbloy@gmail.com>
2020-08-20 21:09:22 +0300

    commit

---
+++ forEach.js
@@ -0,0 +1,5 @@
+function forEach(array){
+    for (let i = 0; i < array.length; i++){
+        //Do something with array[i]
+    }
+}
---
+++ map.js
@@ -0,0 +1,7 @@
+function map(array){
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        newArray.push(array[i])//Any condition or manupulation can be done during this loop
+    }
+    return newArray;
+}
